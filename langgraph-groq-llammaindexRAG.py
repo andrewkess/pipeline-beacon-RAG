5cec77465ad1f31e4b462b65c3e655b0bcaac417
Andrew Kessinger <andrew.kessinger@gmail.com>
2024-07-23 16:19:36 +0200

    dfg

--- langgraph-groq-llammaindexRAG.py
+++ langgraph-groq-llammaindexRAG.py
@@ -176,17 +176,17 @@ class Pipeline:
         # Check if the last message is a ToolMessage and handle it
         if isinstance(last_message, ToolMessage):
             # Process the tool message content
-            # tool_response = AIMessage(content=str(last_message.content))
-            # print(f"Current Messages before appending: {messages}")
-            # messages.append(tool_response)  # Add tool response to the context
-            # print(f"Current Messages after appending: {messages}")
-
-            print(f"Current Messages before sythnesizing tool message: {messages}")
+            tool_response = AIMessage(content=str(last_message.content))
+            print(f"Current Messages before appending: {messages}")
+            messages.append(tool_response)  # Add tool response to the context
+            print(f"Current Messages after appending: {messages}")
 
             # response_content = tool_response
             # Now invoke the LLM with the updated messages list
-            # response_content = self.llm_notools.invoke(messages)
-            response_content = self.llm.invoke(messages)
+            response_content = self.llm_notools.invoke(messages)
+            # response_content = self.llm.invoke(messages)
+            response_content = AIMessage(content=str(response_content))
+
 
         else:
             # If not, invoke the LLM or handle other message types
