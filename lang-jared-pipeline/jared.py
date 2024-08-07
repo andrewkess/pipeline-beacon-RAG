64130fd319c20b16fb4b1a57f05ee7ad33a06e1b
Andrew Kessinger <andrew.kessinger@gmail.com>
2024-08-07 04:26:40 +0200

    color log

--- lang-jared-pipeline/jared.py
+++ lang-jared-pipeline/jared.py
@@ -5,7 +5,7 @@ date: 2024-05-30
 version: 1.0
 license: MIT
 description: A pipeline for interacting with the Jar3d agent using the Langgraph framework.
-requirements: langgraph, termcolor, pydantic, datetime, llama-index
+requirements: langgraph, termcolor, pydantic, datetime, llama-index, colorlog
 """
 
 from typing import List, Union, Generator, Iterator, Any, Dict
